ba408d43547fbc4c32199cb01847b763a5efdfa6
phucnguyen02 <ndhp02@gmail.com>
2023-07-29 20:32:59 -0700

    Removed unnecessary code

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,33 +14,12 @@ function Map({coords, center, zoom, regular}) {
                 map
             });
 
-            let content = document.createElement("div");
-            let h2 = document.createElement("h2");
-            h2.innerText = "Warehouse Info"
-            content.append(h2);
-
-            let ul = document.createElement("ul");
-            content.append(ul);
-
-            let address = document.createElement("li");
-            address.innerText = "Address: " + elem.name;
-            ul.append(address);
-
-            let priceLI = document.createElement("li");
-            let price = regular ? elem.regular_gas : elem.premium_gas;
-            priceLI.innerText = "Price: " + price;
-            ul.append(priceLI);
-
-            let infoWindow = new maps.InfoWindow({
-                content: content
-            })
             marker.addListener("click", () => {
                 setLocationInfo({
                     name: elem.name,
                     regular_gas: elem.regular_gas,
                     premium_gas: elem.premium_gas
                 })
-                //infoWindow.open(map, marker)
             })
             markers.push(marker);
         })
