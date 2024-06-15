76744f58a6c113abd4de73d2fa5b4abfb7660ac4
phucnguyen02 <ndhp02@gmail.com>
2024-06-15 09:53:34 -0700

    Bug fix

--- src/components/Gas.js
+++ src/components/Gas.js
@@ -1,21 +1,19 @@
 import {useState, useContext} from 'react'
 import { RegularContext } from './RegularContext'
 import Marker from './Marker'
-// import LocationInfoBox from './LocationInfoBox'
 import { CoordsContext } from './CoordsContext'
 
 function Gas({map}){
     const {coords, setCoords} = useContext(CoordsContext);
     const [highlight, setHighlight] = useState(false);
     const {isRegular} = useContext(RegularContext);
-    // const [locationInfo, setLocationInfo] = useState(null);
     return (
         <>
             {
                 coords.map((elem, index) => 
                     {
                         return <Marker key = {index} map = {map} address = {elem.address} last_scraped = {elem.last_scraped} last_updated = {elem.last_updated}
-                        position = {elem.position} locationInfo = {locationInfo} setLocationInfo = {setLocationInfo} streetview = {elem.streetview}>
+                        position = {elem.position} streetview = {elem.streetview}>
                             <div className = {`marker ${highlight === index ? "highlight" : ""} ${elem.map_highlight ? "chatbot-highlight" : ""}`}
                                 onMouseEnter = {() => setHighlight(index)}
                                 onMouseLeave = {() => setHighlight(null)}
@@ -26,7 +24,6 @@ function Gas({map}){
                     }
                 )
             }
-            {/* {locationInfo && <LocationInfoBox info = {locationInfo}/>} */}
         </>
     )
 }
