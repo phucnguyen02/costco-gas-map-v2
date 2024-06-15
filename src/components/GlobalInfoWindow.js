93adf0d86503e5b1517e39858b651a184c539610
Nhan Tam <nhantamhd@gmail.com>
2024-06-16 00:15:11 +0700

    Push code

--- src/components/GlobalInfoWindow.js
+++ src/components/GlobalInfoWindow.js
@@ -1,13 +1,11 @@
 // Create a global variable to hold the currently open InfoWindow.
-
-// Update this variable whenever a new InfoWindow is opened.
-
-// Close the previous InfoWindow before opening a new one.
-
 let currentInfoWindow = null;
 
 export function setCurrentInfoWindow(infoWindow) {
+    // Update this variable whenever a new InfoWindow is opened.
     if (currentInfoWindow) {
+        // Close the previous InfoWindow before opening a new one.
+
         currentInfoWindow.close();
     }
     currentInfoWindow = infoWindow;
